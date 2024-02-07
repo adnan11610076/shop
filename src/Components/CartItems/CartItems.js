107d195a2635ca3c61ef2660fa5df622a83fd325
adnan <adnanalam8360@gmail.com>
2024-02-07 12:33:37 +0530

    change

--- src/Components/CartItems/CartItems.js
+++ src/Components/CartItems/CartItems.js
@@ -26,9 +26,9 @@ const CartItems = () => {
                                 <div className="cartitems-format cartitems-format-main">
                                     <img src={e.image} alt="" className="carticon-product-icon" />
                                     <p>{e.name}</p>
-                                    <p>${e.new_price}</p>
+                                    <p>₹{e.new_price}</p>
                                     <button className="cartitems-quantity">{cartItems[e.id]}</button>
-                                    <p>${e.new_price * cartItems[e.id]}</p>
+                                    <p>₹{e.new_price * cartItems[e.id]}</p>
                                     <img
                                         src={remove_icon} className="cartitems-remove-item"
                                         onClick={() => removeFromCart(e.id)}
@@ -47,7 +47,7 @@ const CartItems = () => {
                         <div>
                             <div className="cartitem-total-item">
                                 <p>Subtotal</p>
-                                <p>${getTotalCartAmount()}</p>
+                                <p>₹{getTotalCartAmount()}</p>
                             </div>
                             <hr />
                             <div className="cartitem-total-item">
@@ -57,7 +57,7 @@ const CartItems = () => {
                             <hr />
                             <div className="cartitem-total-item">
                                 <h3>Total</h3>
-                                <h3>${getTotalCartAmount()}</h3>
+                                <h3>₹{getTotalCartAmount()}</h3>
                             </div>
                         </div>
 
